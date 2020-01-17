e33cedad28fa4030cd7de4828d5b5a0d41a55a7c
Dawid Gałęziewski <dgaleziewski@empik.com>
2020-01-17 16:59:15 +0100

    update: started working on carousel js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,32 @@
 import "./main.scss";
 
 // JavaScript bundling
-import "./javascript/test";
+
+const carouselDOM = document.getElementById("carousel-testimonial");
+
+const carouselTestimonial = UICarousel => {
+  UICarousel.addEventListener("click", e => {
+    const profiles = UICarousel.querySelectorAll(
+      ".carousel-testimonial__testimonial-profile"
+    );
+
+    profiles.forEach(profile => {
+      if (
+        profile.classList.contains(
+          ".carousel-testimonial__testimonial-profile--active"
+        )
+      ) {
+        profile.classList.remove(
+          ".carousel-testimonial__testimonial-profile--active"
+        );
+      }
+    });
+
+    const targetProfile = e.target.parentElement.parentElement;
+    console.log(targetProfile);
+  });
+};
+
+carouselTestimonial(carouselDOM);
+
+console.log(carouselDOM);
