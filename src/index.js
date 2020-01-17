cec02846a99f778543651925867bd18a42631881
Dawid Gałęziewski <dgaleziewski@empik.com>
2020-01-17 17:20:12 +0100

    update: working on carouselTestimonial js logic

--- src/index.js
+++ src/index.js
@@ -7,24 +7,21 @@ const carouselDOM = document.getElementById("carousel-testimonial");
 
 const carouselTestimonial = UICarousel => {
   UICarousel.addEventListener("click", e => {
-    const profiles = UICarousel.querySelectorAll(
-      ".carousel-testimonial__testimonial-profile"
+    const profiles = Array.from(
+      UICarousel.querySelectorAll(".carousel-testimonial__testimonial-profile")
     );
 
     profiles.forEach(profile => {
-      if (
-        profile.classList.contains(
-          ".carousel-testimonial__testimonial-profile--active"
-        )
-      ) {
-        profile.classList.remove(
-          ".carousel-testimonial__testimonial-profile--active"
-        );
-      }
+      profile.classList.remove(
+        "carousel-testimonial__testimonial-profile--active"
+      );
     });
 
-    const targetProfile = e.target.parentElement.parentElement;
-    console.log(targetProfile);
+    const targetProfile = e.target.parentElement.parentElement.parentElement;
+
+    targetProfile.classList.add(
+      "carousel-testimonial__testimonial-profile--active"
+    );
   });
 };
 
