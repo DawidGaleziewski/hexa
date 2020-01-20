c343dabad4e15989dc2729cb45128a890de4f0f3
Dawid Gałęziewski <dgaleziewski@empik.com>
2020-01-20 11:57:12 +0100

    update: completed testimonial section

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import "./main.scss";
 // JavaScript bundling
 import CarouselTestimonial from "./javascript/components/carousel-testimonial";
 
-const carouselTestimonial = new CarouselTestimonial("carousel-testimonial");
+const carouselTestimonial = new CarouselTestimonial(
+  "carousel-testimonial",
+  "carousel-testimonial__testimonial-profile",
+  "carousel-testimonial__icon",
+  "carousel-testimonial__testimonial-profile--active"
+);
 
-carouselTestimonial.showID();
+carouselTestimonial.init();
--- src/javascript/components/carousel-testimonial.js
+++ src/javascript/components/carousel-testimonial.js
@@ -1,21 +1,41 @@
 class CarouselTestimonial {
-  constructor(carouselSelectorID, singleProfileQuerySelector) {
+  constructor(
+    carouselSelectorID,
+    singleProfileQuerySelector,
+    iconQuerySelector,
+    activeProfileClass
+  ) {
+    // DOM selectors
     this.carouselSelectorID = carouselSelectorID;
     this.singleProfileQuerySelector = singleProfileQuerySelector;
+    this.iconQuerySelector = iconQuerySelector;
+
+    // DOM elements
+    this.DOMcarousel = document.getElementById(this.carouselSelectorID);
+    this.DOMProfiles = document.getElementsByClassName(
+      singleProfileQuerySelector
+    );
+    this.activeProfileClass = activeProfileClass;
   }
 
-  showID() {
-    console.log(this.carouselSelectorID);
+  onClickHandler() {
+    this.DOMcarousel.addEventListener("click", e => {
+      if (e.target.parentElement.classList.contains(this.iconQuerySelector)) {
+        Array.from(this.DOMProfiles).forEach(profile => {
+          if (profile.classList.contains(this.activeProfileClass)) {
+            profile.classList.remove(this.activeProfileClass);
+          }
+        });
+        e.target.parentElement.parentElement.parentElement.classList.add(
+          this.activeProfileClass
+        );
+      }
+    });
+  }
+
+  init() {
+    this.onClickHandler();
   }
 }
 
 export default CarouselTestimonial;
-
-const objectTest = {
-  name: "test",
-  surname: "test2"
-};
-
-const { name, surname } = objectTest;
-
-console.log(name, surname);
