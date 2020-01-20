ccd60f168bc556f502433bc55575f3e69b64385a
Dawid Gałęziewski <dgaleziewski@empik.com>
2020-01-20 15:06:38 +0100

    added js docs comments

--- src/javascript/components/carousel-profiles.js
+++ src/javascript/components/carousel-profiles.js
@@ -1,4 +1,13 @@
+/** Class creating logic for carousels */
 class CarouselProfiles {
+  /**
+   *
+   * @param {string} carouselID - id by which a carousel will be selected
+   * @param {string} sliderClassName - class name by which a carousel will be selected
+   * @param {string} singleProfileClassName - a single profile element class name - used to position the sildes in the slider
+   * @param {string} btnPreviousClassSelector - element that will be used as previous button
+   * @param {string} btnNextClassSelector - element that will be used as a next button
+   */
   constructor(
     carouselID,
     sliderClassName,
@@ -6,14 +15,14 @@ class CarouselProfiles {
     btnPreviousClassSelector,
     btnNextClassSelector
   ) {
-    //  Input
+    //  params required by constructor
     this.carouselID = carouselID;
     this.sliderClassName = sliderClassName;
     this.singleProfileClassName = singleProfileClassName;
     this.btnPreviousClassSelector = btnPreviousClassSelector;
     this.btnNextClassSelector = btnNextClassSelector;
 
-    // DOM elements
+    // DOM elements created using selectors provided
     this.DOMcarousel = document.getElementById(this.carouselID);
     this.DOMslider = this.DOMcarousel.getElementsByClassName(
       this.sliderClassName
@@ -29,21 +38,26 @@ class CarouselProfiles {
     );
 
     // App state
-    this.translateXState = 0;
+    this.translateXState = 0; // Current slider transform property
 
-    // utils
+    // Utils
     this.profileWidth = this.DOMProfiles[0].offsetWidth;
   }
 
+  /**
+   * Positions profiles ansolute in the silder
+   */
   positionProfilesAbsolute() {
     Array.from(this.DOMProfiles).forEach((profile, index) => {
       profile.style.left = `${profile.offsetWidth * index}px`;
     });
   }
 
+  /**
+   * Listens on the previous button for a click and moves the slider to the left
+   */
   movePreviousHandler() {
     this.DOMBtnPrevious[0].addEventListener("click", () => {
-      console.log(this.translateXState - this.profileWidth);
       if (this.translateXState - this.profileWidth <= 0) {
         this.translateXState += this.profileWidth;
         this.DOMslider[0].style.transform = `translateX(${this.translateXState}px)`;
@@ -51,6 +65,9 @@ class CarouselProfiles {
     });
   }
 
+  /**
+   * Listens on the previous button for a click and moves the slider to the right
+   */
   moveNextHandler() {
     this.DOMBtnNext[0].addEventListener("click", () => {
       if (
@@ -63,6 +80,9 @@ class CarouselProfiles {
     });
   }
 
+  /**
+   * Bootstraps all the functions required by the carousel
+   */
   init() {
     this.positionProfilesAbsolute();
     this.movePreviousHandler();
